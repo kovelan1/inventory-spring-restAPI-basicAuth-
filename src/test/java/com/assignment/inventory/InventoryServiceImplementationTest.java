7c5aa655c8d905d05ac098f844719225ebe2b305
kovelan <kovelan@saastaq.io>
2024-06-05 08:46:23 +0530

    test property file added

--- src/test/java/com/assignment/inventory/InventoryServiceImplementationTest.java
+++ src/test/java/com/assignment/inventory/InventoryServiceImplementationTest.java
@@ -14,6 +14,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Arrays;
@@ -26,6 +27,7 @@ import static org.mockito.Mockito.*;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
 public class InventoryServiceImplementationTest {
 
     @Mock
